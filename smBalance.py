2617ac6849914d826097a9ce9e66ee79524cdbe5
amaur1mmj <amaur1mmj@gmail.com>
2023-07-03 12:40:19 -0300

    atualizações nada agradaveis

--- smBalance.py
+++ smBalance.py
@@ -1,20 +1,24 @@
 from mpi4py import MPI
 
-def soma(inic, fim):
-    # Função para calcular a soma dos valores de inic a fim
-    return sum(range(inic, fim + 1))
+def soma(inicio, fim):
+   s=0
+   for i in range(inicio, fim+1):
+     s=s+i
+   
+   return s   
 
 comm = MPI.COMM_WORLD
 size = comm.Get_size()
 rank = comm.Get_rank()
 
 if rank == 0:
-    pedacos = int(input("Digite o número de pedaços: "))
+    pedacos = size
     valor = int(input("Digite um valor: "))
     parte = valor // pedacos
+    resto = valor % pedacos
 
-    vetinic = [0] * pedacos
-    vetfim = [0] * pedacos
+    vetinic = [i * parte + min(i, resto) + 1 for i in range(pedacos)]
+    vetfim = [(i + 1) * parte + min(i + 1, resto) for i in range(pedacos)]
 
     for i in range(pedacos):
         vetinic[i] = i * parte + 1
@@ -25,24 +29,30 @@ if rank == 0:
 
     foi = 0
 
-    for i in range(1, size):
+    for i in range(size):
         # Envia vetinic[foi] e vetfim[foi] para o processador i
         comm.send(vetinic[foi], dest=i, tag=1)
-        comm.send(vetfim[foi], dest=i, tag=2)
+        print(i, size)
+        comm.send(vetfim[foi], dest=i, tag=2)     
+        
         foi += 1
 
     somapar = 0
-
+    
     while foi <= pedacos - 1:
         # Recebe somaesc de algum processador
-        somaesc = comm.recv(source=MPI.ANY_SOURCE, tag=3)
+        somaesc = comm.recv(source=MPI.ANY_SOURCE)
+        print("soma",somaesc)
         id = MPI.Status().Get_source()
         somapar += somaesc
 
+        print(id)
         # Envia vetinic[foi] e vetfim[foi] para o processador id
         comm.send(vetinic[foi], dest=id, tag=1)
+        
         comm.send(vetfim[foi], dest=id, tag=2)
         foi += 1
+        print('kkkkkkkkkkk')
 
     for i in range(1, size):
         # Recebe somaesc do processador i
@@ -54,6 +64,8 @@ if rank == 0:
 else:
     inic = comm.recv(source=0, tag=1)
     fim = comm.recv(source=0, tag=2)
+    print("inicio", inic)
+    print("fim", fim)
 
     somaesc = soma(inic, fim)
     comm.send(somaesc, dest=0, tag=3)
---
+++ test.py
@@ -0,0 +1,7 @@
+pd = 2
+ok  = [0]* pd
+
+print(ok)
+
+for i in range( pd):
+    print(i)
